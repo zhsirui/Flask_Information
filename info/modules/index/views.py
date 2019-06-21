76537bd7fb9d5e0ab9add4b5702786080a26a778
zhangsirui <zhang_sirui@outlook.com>
2019-06-21 09:36:00 +0800

    加载favicon图标

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,13 @@
+from flask import current_app
+from flask import render_template
+
 from . import index_blu
 from info import redis_store
 
 @index_blu.route('/')
 def index():
+    return render_template('news/index.html')
 
-    return 'index'
+@index_blu.route('/favicon.ico')
+def favcion():
+    return current_app.send_static_file('news/favicon.ico')
