b066b6f2d48a78e4818fe70c64bbe58e8fbef706
zhangsirui <zhang_sirui@outlook.com>
2019-06-25 10:17:00 +0800

    收藏及取消收藏

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,13 +1,59 @@
-from flask import abort
+from flask import abort, jsonify
 from flask import current_app
 from flask import g
 from flask import render_template
+from flask import request
 from flask import session
 
 from info import constants
 from info.models import News, User
 from info.modules.news import news_blu
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
+
+
+@news_blu.route('/news_collect', methods=["POST"])
+@user_login_data
+def collect_news():
+
+    user = g.user
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    news_id = request.json.get("news_id")
+    action = request.json.get("action")
+
+    if not all([news_id, action]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    if action not in ["collect", "cancel_collect"]:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    try:
+        news_id = int(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        return jsonify(errno=RET.DBERR, errmsg="数据查询错误")
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="未查询到新闻数据")
+
+    if action == "cancel_collect":
+        if news in user.collection_news:
+            user.collection_news.remove(news)
+    else:
+        if news not in user.collection_news:
+            user.collection_news.append(news)
+
+    return jsonify(errno=RET.OK, errmsg="操作成功")
+
+
 
 
 @news_blu.route('/<int:news_id>')
@@ -38,10 +84,19 @@ def news_detail(news_id):
 
     news.clicks += 1
 
+    is_collected = False
+
+    if user:
+        if news in user.collection_news:
+            is_collected = True
+
+
+
     data = {
         "user": user.to_dict() if user else None,
         "news_dict_li": news_dict_li,
-        "news": news.to_dict()
+        "news": news.to_dict(),
+        "is_collected": is_collected
     }
 
     return render_template('news/detail.html', data=data)
