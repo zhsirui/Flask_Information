4c4edba4f5a2e196dfbe8a3e1631f2b34e63eaff
zhangsirui <zhang_sirui@outlook.com>
2019-06-27 12:56:01 +0800

    收藏

--- info/constants.py
+++ info/constants.py
@@ -5,7 +5,7 @@ IMAGE_CODE_REDIS_EXPIRES = 300
 SMS_CODE_REDIS_EXPIRES = 300
 
 # 七牛空间域名
-QINIU_DOMIN_PREFIX = "http://oyucyko3w.bkt.clouddn.com/"
+QINIU_DOMIN_PREFIX = "http://ptql19vhd.bkt.clouddn.com/"
 
 # 首页展示最多的新闻数量
 HOME_PAGE_MAX_NEWS = 10
@@ -13,7 +13,7 @@ HOME_PAGE_MAX_NEWS = 10
 # 用户的关注每一页最多数量
 USER_FOLLOWED_MAX_COUNT = 4
 
-# 用户收藏最多新闻数量
+# 用户显示收藏最多新闻数量
 USER_COLLECTION_MAX_NEWS = 10
 
 # 其他用户每一页最多新闻数量
